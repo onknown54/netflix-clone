3e47f6afd7f8f6a8635b42b5e140cf43f7871bd9
onknown54 <lekanm715@gmail.com>
2023-11-02 11:50:37 +0100

    more setup

--- src/helper/utilities.ts
+++ src/helper/utilities.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({
+export const instance = axios.create({
   baseURL: "https://some-domain.com/api/",
   timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
